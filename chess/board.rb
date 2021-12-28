cce8c5a4ecda365fc05237ef60257357cd142282
yakzaetz <yakzaetz@gmail.com>
2021-12-28 13:35:43 -0500

    added a few Board methods

--- chess/board.rb
+++ chess/board.rb
@@ -8,12 +8,12 @@ class Board
 
     def [](pos)
         row,col = pos
-        @grid[row][col]
+        @rows[row][col]
     end
 
     def []=(pos,value)
         row,col = pos
-        @grid[row][col] = value
+        @rows[row][col] = value
     end
     
     def move_piece(color,start_pos,end_pos)
@@ -21,10 +21,32 @@ class Board
         if self[end_pos].nil? || color != self[end_pos].color
             self[end_pos] = piece
             self[start_pos] = NullPiece
-
         end
+    end
+
+    def valid_pos?(pos)
+        row, col = pos
+        (row >= 0 && row < 8 && col >= 0 && col < 8)
+    end
+
+    def add_piece(piece, pos)
+        self[pos] = piece
+    end
+
+    def checkmate?(color)
+        king.color == color && king.valid_moves.empty?
+    end
+
+    def in_check?(color)
         
-        
+    end
+
+    def find_king(color)
+        (0...@rows.length).each do |i|
+            (0...@rows.length).each do |i2|
+                return [i,i2] if @rows[i][i2].is_a?(King) && @rows[i][i2].color == color
+            end 
+        end 
     end
 
     def place_pieces
